d2ddf013ee6ee937005eaae785f3f5d420c07df8
vipul016 <vipulwaskel934@gmail.com>
2023-07-17 19:29:26 +0530

    login added

--- accounts/models.py
+++ accounts/models.py
@@ -71,7 +71,12 @@ class User(AbstractBaseUser):
         return self.is_admin
    def has_module_perms(self,app_label):
         return True
-
+   def get_role(self):
+        if self.role==1:
+            user_role='Restaurant'
+        elif self.role==2:
+            user_role='Customer'
+        return user_role
     
 class UserProfile(models.Model):
     user=OneToOneField(User,on_delete=models.CASCADE,blank=True,null=True)
