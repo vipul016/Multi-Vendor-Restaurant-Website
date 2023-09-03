feae73710a5690294dde0e19d4be09c37ad4d354
vipul016 <vipulwaskel934@gmail.com>
2023-09-03 15:32:57 +0530

    menu builder category crud

--- restaurtWebsite_main/settings.py
+++ restaurtWebsite_main/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'vendor'
+    'vendor',
+    'menu',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +67,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'accounts.context_processors.get_vendor',
+                'accounts.context_processors.get_google_api',
             ],
         },
     },
@@ -154,3 +156,5 @@ EMAIL_HOST_USER=config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD=config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS=True
 DEFAULT_FROM_EMAIL='Multi Vendor Restaurant <multivendor97@gmail.com>'
+
+GOOGLE_API_KEY='AIzaSyC4yTudrg9pTcB2khAgrbCMBfVvbviOhVU'
